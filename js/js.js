62743c4e920bb4261d9b4343621caf150765edc2
Trunutslover <46417890+Trunutslover@users.noreply.github.com>
2019-01-10 18:24:25 +0400

    Добавил переключение слайдов

--- js/js.js
+++ js/js.js
@@ -1,6 +1,10 @@
 var popup = document.querySelector(".popup");
 var popupOpen = document.querySelector(".adress__button");
 var popupClose = popup.querySelector(".write-us__close");
+var sliderToggler = document.querySelector(".slider-controls-group");
+var firstSlide = document.querySelector(".slider__item-1");
+var secondSlide = document.querySelector(".slider__item-2");
+var thirdSlide = document.querySelector(".slider__item-3");
 
 popupOpen.addEventListener ("click", function(evt) {
     evt.preventDefault();
@@ -11,3 +15,19 @@ popupClose.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("popup-show");
 });
+
+sliderToggler.addEventListener("click", function () {
+    if (document.slider.slider__controls[0].checked) {
+        firstSlide.classList.add("slider-show");
+        secondSlide.classList.remove("slider-show");
+        thirdSlide.classList.remove("slider-show");
+    } else if (document.slider.slider__controls[1].checked) {
+        secondSlide.classList.add("slider-show");
+        firstSlide.classList.remove("slider-show");
+        thirdSlide.classList.remove("slider-show");
+    } else if (document.slider.slider__controls[2].checked) {
+        thirdSlide.classList.add("slider-show");
+        firstSlide.classList.remove("slider-show");
+        secondSlide.classList.remove("slider-show");
+    }
+})
